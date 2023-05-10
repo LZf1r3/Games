544933a340f0ca49af2835f4f809805d18680209
LZf1r3 <lucas.mfaiad@gmail.com>
2023-05-09 21:44:46 -0700

    Create Find the number.py

---
+++ Find the number.py
@@ -0,0 +1,8 @@
+# Find The Number Game
+# Made by: Lucas Faiad
+# Contact: lucas.mfaiad@gmail.com
+###################################
+
+## Rules ##
+# Your objective is to guess the number chosen by the coomputer.
+# The number of tries depends on the level that you choose Easy(20) Medium(10) Hard(5)
